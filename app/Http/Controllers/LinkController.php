426e7d3d5624ffcfb547e39a9bdb6abc8e4d6145
AlexanderAlatorreGovea <alexander_alatorre1993@hotmail.com>
2021-06-24 09:58:22 -0700

    added migrations, seeders, and Controllers

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\link;
+use Illuminate\Http\Request;
+
+class LinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\link  $link
+     * @return \Illuminate\Http\Response
+     */
+    public function show(link $link)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\link  $link
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(link $link)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\link  $link
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, link $link)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\link  $link
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(link $link)
+    {
+        //
+    }
+}
